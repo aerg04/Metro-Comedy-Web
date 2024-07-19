f5b5219e13e4c2e0b1bf08a2557d76b4d774299f
Andres Rangel <andres.rangel@correo.unimet.edu.ve>
2024-07-18 21:07:25 -0400

    tailwind funcional y componente

---
+++ src/components/ContainerCarousel.jsx
@@ -0,0 +1,21 @@
+
+export default function ContainerCarousel() {
+    return(
+        <>
+            <div className="flex flex-col text-white">
+                <div className="w-auto h-64 flex overflow-hidden rounded-t-lg">
+                    <img className="w-48 h-auto object-cover object-position"
+                    src="src/assets/yo-Alejandro-Holguin.jpg" alt="" />
+
+                </div>
+                <div className="bg-orange-400 justify-center items-center flex flex-col gap-y-1.5  lg:h-4 p-2 rounded-b-lg">
+                   <div>
+                        <p className="font-bold">Alejandro Holguin</p>
+                        <p className="">Especial On Demand</p>
+                   </div>
+                    <button className="bg-black rounded-full px-2 py-1 font-bold"> Comprar </button>
+                </div>
+            </div>  
+        </>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import ContainerCarousel from "../components/ContainerCarousel";
+
+export default function Home() {    
+    return (
+        <div className="flex h-screen justify-center items-center">
+            <ContainerCarousel />
+        </div>
+    )
+}
