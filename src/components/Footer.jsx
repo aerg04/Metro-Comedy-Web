7a7a6abd67e524a47c6f966858eb90aba9cc7b4d
Andres Rangel <andres.rangel@correo.unimet.edu.ve>
2024-07-31 16:23:04 -0400

    Correcion bug footer acordeon

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+
+export default function Footer(){
+    return(
+        <footer className="h-20 w-full bg-black flex justify-center items-center">
+            <p className="text-white">© 2024 MetroComedy</p>
+        </footer>
+    );
+}
